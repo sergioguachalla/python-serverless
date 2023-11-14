9a2ee6e3127b0b8d0d67adf648fd21f9680df50f
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-11-13 20:38:54 -0400

    first commit

---
+++ handler.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import uuid
+
+
+
+def hello(event, context):
+    table_name = 'users'
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    result = None
+
+    people = [
+            { 'id' : '1', 'name' : 'Martha', 'lastname' : 'Rivera'},
+            { 'id' : '2', 'name' : 'Nikki', 'lastname' : 'Wolf'},
+            { 'id' : '3', 'name' : 'John', 'lastname' : 'Doe'},
+        ]
+    
+    with table.batch_writer() as batch_writer:
+        for person in people:
+            item = {
+                'id' : person['id'],
+                'name'  : person['name'],
+                'lastname': person['lastname']
+            }
+            print("> batch writing: {}".format(person['name']) )
+            batch_writer.put_item(Item=item)
+            
+        result = f"Success. Added {len(people)} people to {table_name}."
+
+    return {'message': result}
